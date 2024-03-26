a20a062d4e23bc9cb3b06791ba31a641590e9c0d
dakshj31 <dakshjoshi1355@gmail.com>
2024-03-26 22:56:38 +0530

    13

--- resources/functions.php
+++ resources/functions.php
@@ -265,5 +265,44 @@ echo $product;
 
 /********************************ADD PRODUCTS IN ADMIN************************************/
 
+function add_product() {
+
+    if(isset($_POST['publish'])) {
+
+        $product_title             = escape_string($_POST['product_title']);
+        $product_category_id       = escape_string($_POST['product_category_id']);
+        $product_price             = escape_string($_POST['product_price']);
+        $product_description       = escape_string($_POST['product_description']);
+        $short_desc                = escape_string($_POST['short_desc']);
+        $product_quantity          = escape_string($_POST['product_quantity']);
+        $product_image             = escape_string($_FILES['file']['name']);
+        $image_temp_location       = escape_string($_FILES['file']['tmp_name']);
+
+        move_uploaded_file($image_temp_location, UPLOAD_DIRECTORY . DS . $product_image);
+
+        $query = query("INSERT INTO products(product_title, product_category_id, product_price, product_description, short_desc, product_quantity, product_image) VALUES('{$product_title}', '{$product_category_id}', '{$product_price}', '{$product_description}', '{$short_desc}', '{$product_quantity}', '{$product_image}')");
+        $last_id = last_id();
+        confirm($query);
+        set_message("New Product with id {$last_id} was Added");
+        redirect("index.php?products");
+    
+    
+    }
+    
+}
+
+
+function show_categories_add_product_page() {
+
+    $query = query("SELECT * FROM categories");
+               confirm($query);
+                while($row = fetch_array($query)) {
+                   $category_links = <<<DELIMETER
+                   <option value="{$row['cat_id']}">{$row['cat_title']}</option>
+                   DELIMETER; 
+                
+                echo $category_links;
+                }
+}
 
 ?>
--- resources/templates/back/add_product.php
+++ resources/templates/back/add_product.php
@@ -1,3 +1,5 @@
+<?php require_once("../../resources/config.php"); ?> 
+<?php add_product(); ?> 
 <div class="col-md-12">
 
 <div class="row">
@@ -36,21 +38,16 @@
       </div>
     </div>
 
-
-
-
-    
-    
+    <div class="form-group">
+           <label for="product-title">Product Short Description</label>
+      <textarea name="short_desc" id="" cols="30" rows="3" class="form-control"></textarea>
+    </div>
 
 </div><!--Main Content-->
 
 
 <!-- SIDEBAR-->
-
-
-<aside id="admin_sidebar" class="col-md-4">
-
-     
+<aside id="admin_sidebar" class="col-md-4">     
      <div class="form-group">
        <input type="submit" name="draft" class="btn btn-warning btn-lg" value="Draft">
         <input type="submit" name="publish" class="btn btn-primary btn-lg" value="Publish">
@@ -61,38 +58,32 @@
 
     <div class="form-group">
          <label for="product-title">Product Category</label>
-          <hr>
-        <select name="product_category" id="" class="form-control">
+  
+        <select name="product_category_id" id="" class="form-control">
             <option value="">Select Category</option>
-           
+           <?php show_categories_add_product_page(); ?>
         </select>
 
 
 </div>
 
-
-
-
-
     <!-- Product Brands-->
 
 
     <div class="form-group">
-      <label for="product-title">Product Brand</label>
-         <select name="product_brand" id="" class="form-control">
-            <option value="">Select Brand</option>
-         </select>
+      <label for="product-title">Product Quantity</label>
+         <input type="number" name="product_quantity" class="form-control">
     </div>
 
 
 <!-- Product Tags -->
 
-
+<!-- 
     <div class="form-group">
           <label for="product-title">Product Keywords</label>
           <hr>
         <input type="text" name="product_tags" class="form-control">
-    </div>
+    </div> -->
 
     <!-- Product Image -->
     <div class="form-group">
